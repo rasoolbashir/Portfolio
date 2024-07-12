ac918e10d0830a9ccd7fb0a51d73ba6528295fcf
rasool bashir <bashirrasool0@gmail.com>
2024-07-12 13:10:59 +0500

    working on my personal portfolio

---
+++ Personal portfolio/app.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Front-End Developer", "Graphic Designer", "Freelencer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    startDelay: 0,
+    backDelay: 0,
+    loop: true,
+    loopCount: Infinity // Optional, ensures infinite looping explicitly
+});
